783e11f00e8c4d639c30c19fea2c7ccee0eb1942
Tommy Memed <tommymemed@gmail.com>
2025-07-31 10:00:33 +0100

    xx

--- Temperance.Services/Services/Interfaces/ITransactionCostService.cs
+++ Temperance.Services/Services/Interfaces/ITransactionCostService.cs
@@ -12,5 +12,10 @@ namespace Temperance.Services.Services.Interfaces
         Task<double> GetSpreadCost(double price, int quantity, string symbol, string interval, DateTime timestamp);
         Task<double> GetSpreadCost(double price, double quantity, string symbol, string interval, DateTime timestamp);
         Task<double> CalculateTotalTradeCost(double entryPrice, double exitPrice, SignalDecision entrySignal, PositionDirection exitPositionDirection, int quantity, string symbol, string interval, DateTime entryTimestamp, DateTime exitTimestamp);
+
+        Task<double> CalculateTotalTradeCost(double entryPrice, double exitPrice, SignalDecision entrySignal, PositionDirection exitPositionDirection, double quantity, string symbol, string interval, DateTime entryTimestamp, DateTime exitTimestamp);
+        Task<double> CalculateCommissionCost(double price, double quantity, string symbol, string interval, DateTime timestamp);
+        Task<double> CalculateSlippageCost(double price, double quantity, PositionDirection direction, string symbol, string interval, DateTime timestamp);
+        Task<double> CalculateOtherCost(double price, double quantity, string symbol, string interval, DateTime timestamp);
     }
 }
